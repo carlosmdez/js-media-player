c8b0e3cec4b5897f06de05b370de35f24801c29a
Carlos Méndez <carlos.mdez@outlook.com>
2020-01-19 21:37:23 -0600

    Serive Workers were added.

--- js/index.js
+++ js/index.js
@@ -11,3 +11,9 @@ const player = new MediaPlayer({ el: video, plugins })
 
 playButton.onclick = () => player.togglePlay()
 muteButton.onclick = () => player.toggleMute()
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../sw.js').catch(err => {
+    console.log(err)
+  })
+}
---
+++ sw.js
@@ -0,0 +1,46 @@
+const VERSION = 'v1'
+
+self.addEventListener('install', event => {
+  event.waitUntil(precache())
+})
+
+self.addEventListener('fetch', event => {
+  const request = event.request
+  //Get
+  if (request.method !== 'GET') {
+    return
+  }
+
+  //Buscar en caché
+  event.respondWith(cachedResponse(request))
+
+  //Actualizar el caché
+  event.waitUntil(updateCache(request))
+})
+
+const precache = async () => {
+  const cache = await caches.open(VERSION)
+  cache.addAll([
+    '/',
+    '/index.html',
+    '/js/index.js',
+    '/js/MediaPlayer.js',
+    '/js/plugins/AutoPause.js',
+    '/js/plugins/AutoPlay.js',
+    '/css/normalize.css',
+    '/css/styles.css',
+    '/assets/video/BigBuckBunny.mp4'
+  ])
+}
+
+const cachedResponse = async request => {
+  const cache = await caches.open(VERSION)
+  const response = await cache.match(request)
+  return response || fetch(request)
+}
+
+const updateCache = async request => {
+  const cache = await caches.open(VERSION)
+  const response = await fetch(request)
+  return cache.put(request, response)
+}
