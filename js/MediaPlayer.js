fb684a3561076f9dec1624d6b1b863f621ad4a15
Carlos Méndez <carlos.mdez@outlook.com>
2020-01-22 23:07:55 -0600

    MediaPlayer file was updated.

--- js/MediaPlayer.js
+++ js/MediaPlayer.js
@@ -1,57 +1,50 @@
-function MediaPlayer(config) {
-  this.media = config.el
-  this.plugins = config.plugins || []
-
-  this._initPlugins()
-  this.mute()
-}
-
-MediaPlayer.prototype._initPlugins = function() {
-  const player = {
-    play: () => this.play(),
-    pause: () => this.pause(),
-    element: this.media,
-    get muted() {
-      return this.element.muted
-    },
-    set muted(value) {
-      this.element = value
+class MediaPlayer {
+  constructor(config) {
+    this.media = config.el
+    this.plugins = config.plugins || []
+    this._initPlugins()
+    this.mute()
+  }
+  _initPlugins() {
+    const player = {
+      play: () => this.play(),
+      pause: () => this.pause(),
+      element: this.media,
+      get muted() {
+        return this.element.muted
+      },
+      set muted(value) {
+        this.element = value
+      }
     }
+    this.plugins.forEach(plugin => {
+      plugin.run(player)
+    })
   }
-
-  this.plugins.forEach(plugin => {
-    plugin.run(player)
-  })
-}
-
-MediaPlayer.prototype.play = function() {
-  this.media.play()
-}
-
-MediaPlayer.prototype.pause = function() {
-  this.media.pause()
-}
-
-MediaPlayer.prototype.togglePlay = function() {
-  if (this.media.paused) {
-    return this.play()
+  play() {
+    this.media.play()
   }
-  return this.pause()
-}
-
-MediaPlayer.prototype.mute = function() {
-  this.media.muted = true
-}
-
-MediaPlayer.prototype.unmute = function() {
-  this.media.muted = false
-}
-
-MediaPlayer.prototype.toggleMute = function() {
-  if (this.media.muted) {
-    return this.unmute()
+  pause() {
+    this.media.pause()
   }
-  return this.mute()
+  togglePlay() {
+    if (this.media.paused) {
+      return this.play()
+    }
+    return this.pause()
+  }
+  mute() {
+    this.media.muted = true
+  }
+  unmute() {
+    this.media.muted = false
+  }
+  toggleMute() {
+    if (this.media.muted) {
+      return this.unmute()
+    }
+    return this.mute()
+  }
 }
 
 export default MediaPlayer
